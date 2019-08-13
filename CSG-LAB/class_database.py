8f6904add8f40880314464eea4749013b6f58656
karan421 <53856783+karan421@users.noreply.github.com>
2019-08-13 15:23:02 +0530

    Adding class_database.py

---
+++ CSG-LAB/class_database.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+class credentials_database():
+
+    def __init__(self, list):
+        self.newlist = list
+
+    """ create a database connection to the SQLite database"""
+    def connectdb(self):
+        con = sqlite3.connect("KCLAB.db")
+        return con
+
+    """ Insert Credentials into the SQLite database"""
+    def insert_credentials(self, con):
+        cur = con.cursor()
+        print("Adding Credentials in database: " + self.newlist[1])
+        try:
+
+            cur.execute("insert into CREDENTIALS (name, lastname, mail, password) VALUES(?,?,?,?)",(self.newlist[0], self.newlist[1], self.newlist[2], self.newlist[3]))
+            con.commit()
+            print("Credentials added successfully")
+            return "SUCCESS"
+        except Exception as e:
+            print(e)
+            return "FAIL"
+
+    def search_credentials(self, con):
+        cur = con.cursor()
+        count = cur.execute('select count(mail) from CREDENTIALS where mail=""')
+
+    def getcredentials(self, con):
+        cur = con.cursor()
+        print("Fetching Credentials detail for User: " + str(self.newlist[0]))
+        output = cur.execute("select * from Credentials where mail=? AND password=?", [self.newlist[0], self.newlist[1]])
+        return output
+
+
+class getcredentials():
+    def connectdb(self):
+        con = sqlite3.connect("KCLAB.db")
+        return con
+
+    def get_name(self, con, mailid):
+        cur = con.cursor()
+        print("Fetching Name & Surname ")
+        output = cur.execute("select name, lastname from Credentials where mail=?", [mailid])
+        name = []
+        for val in output:
+            name.append(val[0])
+            name.append(val[1])
+        print(name)
+        return name
